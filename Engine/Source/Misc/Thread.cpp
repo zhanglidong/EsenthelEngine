bdf5acd70ddeaa9e63eeea16e514b41b44ac1317
GregSlazinski <esenthel@hotmail.com>
2019-03-15 23:27:44 +0700

    ok

--- Engine/Source/Misc/Thread.cpp
+++ Engine/Source/Misc/Thread.cpp
@@ -187,7 +187,7 @@ SyncLock::SyncLock()
 #else
                    pthread_mutexattr_t                attr;
    DYNAMIC_ASSERT(!pthread_mutexattr_init   (        &attr                         ), "pthread_mutexattr_init");
-     DEBUG_ASSERT(!pthread_mutexattr_settype(        &attr, PTHREAD_MUTEX_RECURSIVE), "pthread_mutexattr_settype"); // this can fail but only if parameter is invalid, and since PTHREAD_MUTEX_RECURSIVE is valid, then do error checking only in Debug
+        int result=pthread_mutexattr_settype(        &attr, PTHREAD_MUTEX_RECURSIVE); DEBUG_ASSERT(!result, "pthread_mutexattr_settype"); // this can fail but only if parameter is invalid, and since PTHREAD_MUTEX_RECURSIVE is valid, then do error checking only in Debug
    DYNAMIC_ASSERT(!pthread_mutex_init       (&_lock, &attr                         ), "pthread_mutex_init");
                    pthread_mutexattr_destroy(        &attr                         ); // skip error checking for this one
 #endif
