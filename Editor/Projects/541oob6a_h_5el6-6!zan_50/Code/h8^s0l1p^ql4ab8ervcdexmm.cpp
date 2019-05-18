a283a8e24a06959194f1ba3235d78226310d1b40
GregSlazinski <esenthel@hotmail.com>
2019-05-18 19:30:06 +0700

    ok

--- Editor/Projects/541oob6a_h_5el6-6!zan_50/Code/h8^s0l1p^ql4ab8ervcdexmm.cpp
+++ Editor/Projects/541oob6a_h_5el6-6!zan_50/Code/h8^s0l1p^ql4ab8ervcdexmm.cpp
@@ -8,7 +8,7 @@ bool Init()
    Ftp ftp; // create and Ftp object
 
    // login to the FTP
-   if(ftp.login("ftp.host.com", "user", "password")) // in order to connect to an FTP server you will need to change these parameters to your custom
+   if(ftp.logIn("ftp.host.com", "user", "password")) // in order to connect to an FTP server you will need to change these parameters to your custom
    {
       File file;
 
@@ -25,7 +25,7 @@ bool Init()
       ftp.listFiles("/", files); // list all files in the root "/" ftp to the 'files' container
 
       // logout
-      ftp.logout();
+      ftp.logOut();
    }
 
    return true;
