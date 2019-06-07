f7e9ca5cdfad6b34d1d79c89317db7864ff75f8e
GregSlazinski <esenthel@hotmail.com>
2019-06-07 13:32:25 +0700

    ok

--- ThirdPartyLibs/PVRTC/Windows/clean.bat
+++ ThirdPartyLibs/PVRTC/Windows/clean.bat
@@ -5,11 +5,13 @@ rmdir /s /q Debug
 del Release\*.obj
 del Release\*.log
 del Release\*.tlog
+del Release\*.txt
 del Release\*.lastbuildstate
 rmdir /s /q Release\pvrtc.tlog
 
 del x64\Release\*.obj
 del x64\Release\*.log
 del x64\Release\*.tlog
+del x64\Release\*.txt
 del x64\Release\*.lastbuildstate
 rmdir /s /q x64\Release\pvrtc.tlog
