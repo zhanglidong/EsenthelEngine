4940c1a9e1fbbeedbcf50187a58b5e2ebfbcbb7b
GregSlazinski <esenthel@hotmail.com>
2019-06-07 13:51:42 +0700

    ok

--- ThirdPartyLibs/PVRTC/Windows/VS 2015 Missing Symbols/clean.bat
+++ ThirdPartyLibs/PVRTC/Windows/VS 2015 Missing Symbols/clean.bat
@@ -1,8 +1,16 @@
 rmdir /s /q .vs
 del *.sdf
+
 del Release\*.obj
 del Release\*.log
-rmdir /s /q "Release\VS 2015 .791AA268.tlog"
+del Release\*.tlog
+del Release\*.txt
+del Release\*.lastbuildstate
+del Release\*.unsuccessfulbuild
+
 del x64\Release\*.obj
 del x64\Release\*.log
-rmdir /s /q "x64\Release\VS 2015 .791AA268.tlog"
+del x64\Release\*.tlog
+del x64\Release\*.txt
+del x64\Release\*.lastbuildstate
+del x64\Release\*.unsuccessfulbuild
