346a9b7d374b16a74c8224898c6e1fee9b16ff2c
GregSlazinski <esenthel@hotmail.com>
2019-06-08 19:33:43 +0700

    ok

--- Engine/Source/Graphics/Image Operations.cpp
+++ Engine/Source/Graphics/Image Operations.cpp
@@ -3106,7 +3106,7 @@ Bool Image::blurCubeMipMaps(Flt angle_start, Flt angle_growth, Threads *threads)
       Image *img=this, temp;
       if(img->mode()!=IMAGE_SOFT_CUBE || img->compressed())if(img->copyTry(temp, -1, -1, -1, ImageTypeUncompressed(img->type()), IMAGE_SOFT_CUBE))img=&temp;else return false;
       for(Int i=1; i<img->mipMaps(); i++)BlurCube(*img, i-1, *img, i, angle_start*Pow(i, angle_growth), threads); // or angle_start*(Pow(2.0f, i)-1) with angle_growth somewhere
-      if(img!=this)if(img->copyTry(*img, -1, -1, -1, -1, mode()))Swap(T, *img);else return false; // convert to original mode
+      if(img!=this && !img->copyTry(T, -1, -1, -1, type(), mode()))return false; // convert to original type and mode
    }
    return true;
 }
