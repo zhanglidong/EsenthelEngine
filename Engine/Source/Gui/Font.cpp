20225e37e1298683882c895080dd2c09d56cd3d7
GregSlazinski <esenthel@hotmail.com>
2019-06-22 11:52:08 +0700

    ok

--- Engine/Source/Gui/Font.cpp
+++ Engine/Source/Gui/Font.cpp
@@ -20,6 +20,11 @@
    #pragma message("!! Warning: Use this only for debugging !!")
 #endif
 
+#define FONT_SRGB 0
+
+#define IMAGE_R8G8B8A8_FONT (FONT_SRGB ? IMAGE_R8G8B8A8_SRGB : IMAGE_R8G8B8A8)
+#define IMAGE_L8A8_FONT     (FONT_SRGB ? IMAGE_L8A8_SRGB     : IMAGE_L8A8    )
+
 namespace EE{
 /******************************************************************************/
 #define CC4_FONT CC4('F','O','N','T')
@@ -254,6 +259,7 @@ Bool Font::imageType(IMAGE_TYPE type)
 {
    if(_sub_pixel && type!=IMAGE_R8G8B8A8      && type!=IMAGE_B8G8R8A8      && type!=IMAGE_BC2      && type!=IMAGE_BC3      && type!=IMAGE_BC7      && type!=IMAGE_ETC2_A8
                  && type!=IMAGE_R8G8B8A8_SRGB && type!=IMAGE_B8G8R8A8_SRGB && type!=IMAGE_BC2_SRGB && type!=IMAGE_BC3_SRGB && type!=IMAGE_BC7_SRGB && type!=IMAGE_ETC2_A8_SRGB)return false; // sub pixel supports only these formats
+   type=(FONT_SRGB ? ImageTypeIncludeSRGB : ImageTypeExcludeSRGB)(type);
    Bool changed=false;
    REPA(_images)
    {
@@ -275,7 +281,7 @@ Bool Font::imageType(IMAGE_TYPE type)
 }
 void Font::toSoft()
 {
-   IMAGE_TYPE type=(_sub_pixel ? IMAGE_R8G8B8A8_SRGB : IMAGE_L8A8_SRGB);
+   IMAGE_TYPE type=(_sub_pixel ? IMAGE_R8G8B8A8_FONT : IMAGE_L8A8_FONT);
    REPA(_images)
    {
       Image &image=_images[i];
@@ -404,7 +410,7 @@ static void SwapGreenAlpha(Font &font)
 {
    if(!font._sub_pixel)REPA(font._images)
    {
-      Image &image=font._images[i]; if(image.copyTry(image, -1, -1, -1, IMAGE_L8A8_SRGB, -1, -1, FILTER_NONE)) // disable filtering here as it will be done below
+      Image &image=font._images[i]; if(image.copyTry(image, -1, -1, -1, IMAGE_L8A8_FONT, -1, -1, FILTER_NONE)) // disable filtering here as it will be done below
       {
          if(image.lock())
          {
@@ -709,7 +715,7 @@ struct SystemFontDrawContext
    #if USE_FREE_TYPE
       if(!image.is())
       {
-         image.createSoft(w, h, 1, IMAGE_R8G8B8A8_SRGB);
+         image.createSoft(w, h, 1, IMAGE_R8G8B8A8_FONT);
          if(!FT_Init_FreeType(&library))
          {
             FT_Library_SetLcdFilter(library, FT_LCD_FILTER_DEFAULT);
@@ -732,7 +738,7 @@ struct SystemFontDrawContext
    #elif WINDOWS_OLD
       if(!bitmap)
       {
-         image.createSoft(w, h, 1, IMAGE_R8G8B8A8_SRGB);
+         image.createSoft(w, h, 1, IMAGE_R8G8B8A8_FONT);
 
          BITMAPV5HEADER bi; Zero(bi);
          bi.bV5Size    =SIZE(bi);
@@ -756,8 +762,8 @@ struct SystemFontDrawContext
    #elif MAC
       if(!context)
       {
-         if(!image       .is())       image.createSoft(w, h, 1, IMAGE_R8G8B8A8_SRGB);
-         if(!bitmap_image.is())bitmap_image.createSoft(w, h, 1, IMAGE_R8G8B8A8_SRGB);
+         if(!image       .is())       image.createSoft(w, h, 1, IMAGE_R8G8B8A8_FONT);
+         if(!bitmap_image.is())bitmap_image.createSoft(w, h, 1, IMAGE_R8G8B8A8_FONT);
          if(!bitmap)
          {
             unsigned char *image_data=bitmap_image.data();
@@ -910,7 +916,7 @@ struct FontCreate : Font::Params
    VecI2                            draw_size; // size of the buffer used for drawing
    Memc<FontChar>                   chars; // chars to create
    Int                              processed;   Int leftToProcess()C {return Max(0, chars.elms()-processed-SPECIAL_CHARS);} // skip special chars
-   IMAGE_TYPE                       imageTypeTemp()C {return (mode==Font::SUB_PIXEL) ? IMAGE_R8G8B8A8_SRGB : IMAGE_L8A8_SRGB;}
+   IMAGE_TYPE                       imageTypeTemp()C {return (mode==Font::SUB_PIXEL) ? IMAGE_R8G8B8A8_FONT : IMAGE_L8A8_FONT;}
    SystemFont                       font;
    MemtN<SystemFontDrawContext, 16> dcs;
 
@@ -923,9 +929,12 @@ struct FontCreate : Font::Params
       scaled_size=RoundU(size*scale);
       if(mode==Font::SUB_PIXEL)
       {
-         image_type=IMAGE_R8G8B8A8_SRGB;
+         image_type=IMAGE_R8G8B8A8_FONT;
            mip_maps=1;
         shadow_opacity=0; // disable shadows for sub pixels
+      }else
+      {
+         image_type=(FONT_SRGB ? ImageTypeIncludeSRGB : ImageTypeExcludeSRGB)(image_type);
       }
       Clamp(max_image_size, 0, 65536);
       if(image_type==IMAGE_PVRTC1_2 || image_type==IMAGE_PVRTC1_4 || image_type==IMAGE_PVRTC1_2_SRGB || image_type==IMAGE_PVRTC1_4_SRGB)mip_maps=1; // PVRTC has too low quality to enable mip-maps
