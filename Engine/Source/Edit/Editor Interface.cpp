b1ae58694e850b1016e4f7269750d5e2f06be93b
GregSlazinski <esenthel@hotmail.com>
2019-09-14 17:15:35 +0700

    ok

--- Engine/Source/Edit/Editor Interface.cpp
+++ Engine/Source/Edit/Editor Interface.cpp
@@ -4,7 +4,7 @@ namespace EE{
 static Int Compare(C Edit::Elm &elm, C UID &id) {return Compare(elm.id, id);}
 namespace Edit{
 /******************************************************************************/
-#define EI_VER 35 // this needs to be increased every time a new command is added, existing one is changed, or some of engine class file formats get updated
+#define EI_VER 36 // this needs to be increased every time a new command is added, existing one is changed, or some of engine class file formats get updated
 #define EI_STR "Esenthel Editor Network Interface"
 #define CLIENT_WAIT_TIME         (   60*1000) //    60 seconds
 #define CLIENT_WAIT_TIME_LONG    ( 6*60*1000) //  6*60 seconds, some operations may take a long time to complete
