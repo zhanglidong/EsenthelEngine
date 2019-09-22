b4b3e43996587c3f6b30b54633b855961ed90dca
GregSlazinski <esenthel@hotmail.com>
2019-09-22 13:58:31 +0700

    ok

--- Engine/Source/Shaders/SMAA.cpp
+++ Engine/Source/Shaders/SMAA.cpp
@@ -5,7 +5,7 @@ BUFFER(SMAA)
    Flt SMAAThreshold=0.05;
 BUFFER_END
 
-#define SMAA_HLSL_4 1 // TODO: using SMAA_HLSL_4_1 would be faster, but it's not easy to mix SMAA 4.0 and 4.1 together in one shader, however it's only used for predication and SMAADepthEdgeDetectionPS which are not used
+#define SMAA_HLSL_4 1 // TODO: using SMAA_HLSL_4_1 would be faster, however it's only used for predication and 'SMAADepthEdgeDetectionPS' which are not used
 #define PointSampler  SamplerPoint
 #define LinearSampler SamplerLinearClamp
 
