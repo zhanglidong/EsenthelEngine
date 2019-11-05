13608770b3089b1dd10d80a8e79c3d452f813916
GregSlazinski <esenthel@hotmail.com>
2019-11-05 22:45:16 +0700

    ok

--- Editor Source/_Build_/Esenthel/Source/Shared/Elements/Material.cpp
+++ Editor Source/_Build_/Esenthel/Source/Shared/Elements/Material.cpp
@@ -46,7 +46,9 @@
       {
          case MTECH_ALPHA_TEST:
          case MTECH_GRASS     :
-         case MTECH_LEAF      : color_s.w=0.5f; break;
+         case MTECH_GRASS_3D  :
+         case MTECH_LEAF      :
+            color_s.w=0.5f; break;
 
          default: color_s.w=1; break;
       }
--- Editor Source/h3kv^1fvcwe4ri0#ll#761m7/Code/7x-kgcur3za4pkgmatay1srl.cpp
+++ Editor Source/h3kv^1fvcwe4ri0#ll#761m7/Code/7x-kgcur3za4pkgmatay1srl.cpp
@@ -67,7 +67,9 @@ class EditMaterial
       {
          case MTECH_ALPHA_TEST:
          case MTECH_GRASS     :
-         case MTECH_LEAF      : color_s.w=0.5; break;
+         case MTECH_GRASS_3D  :
+         case MTECH_LEAF      :
+            color_s.w=0.5; break;
 
          default: color_s.w=1; break;
       }
