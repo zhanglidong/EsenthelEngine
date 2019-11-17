08b1f54e5edf4dcd50d128256b0a1cb38ab74ccc
GregSlazinski <esenthel@hotmail.com>
2019-11-17 13:30:35 +0700

    ok

--- Editor Source/_Build_/Esenthel/Source/@@headers.h
+++ Editor Source/_Build_/Esenthel/Source/@@headers.h
@@ -637,8 +637,8 @@ const int           ForceInstaller=-2, // -2=disable and don't update, -1=disabl
                     MeshSplitMinSize=4;
 const uint          MeshJoinAllTestVtxFlag=VTX_HLP|VTX_SIZE;
 const uint          NewElmTime=1;
-const uint ProjectVersion     =66, // !! increase this by one if any of engine/editor asset formats have changed !!
-           ClientServerVersion=64;
+const uint ProjectVersion     =67, // !! increase this by one if any of engine/editor asset formats have changed !!
+           ClientServerVersion=65;
 const int           ServerNetworkCompressionLevel=9           , ClientNetworkCompressionLevel=5            , EsenthelProjectCompressionLevel=5;
 /******************************************************************************/
 // TYPEDEFS
