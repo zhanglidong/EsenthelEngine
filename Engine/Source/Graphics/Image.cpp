75c02a9ec253896cec63e3ad0af6e86982173b15
GregSlazinski <esenthel@hotmail.com>
2019-11-22 14:09:22 +0700

    ok

--- Engine/Source/Graphics/Image.cpp
+++ Engine/Source/Graphics/Image.cpp
@@ -50,6 +50,10 @@ namespace EE{
 #define GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 0x9277
 #define GL_COMPRESSED_RGBA8_ETC2_EAC                 0x9278
 #define GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC          0x9279
+#define GL_COMPRESSED_R11_EAC                        0x9270
+#define GL_COMPRESSED_SIGNED_R11_EAC                 0x9271
+#define GL_COMPRESSED_RG11_EAC                       0x9272
+#define GL_COMPRESSED_SIGNED_RG11_EAC                0x9273
 #define GL_COMPRESSED_RED_RGTC1                      0x8DBB
 #define GL_COMPRESSED_RG_RGTC2                       0x8DBD
 #define GL_COMPRESSED_SIGNED_RED_RGTC1               0x8DBC
