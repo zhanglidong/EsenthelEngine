5fc812d068aceedcecc1554235c01aa9773295f6
GregSlazinski <esenthel@hotmail.com>
2020-11-22 22:41:11 +0700

    ok

--- Engine/Source/Net/Xbox Live.cpp
+++ Engine/Source/Net/Xbox Live.cpp
@@ -17,11 +17,6 @@
 #endif
 
 namespace EE{
-/******************************************************************************
-#if EE_PRIVATE
-   extern std::shared_ptr<xbox::services::system::xbox_live_user> XboxUser;
-   extern std::shared_ptr<xbox::services::xbox_live_context     > XboxCtx;
-#endif
 /******************************************************************************/
 XBOXLive XboxLive;
 
