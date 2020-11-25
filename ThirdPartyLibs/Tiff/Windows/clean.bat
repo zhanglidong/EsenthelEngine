457cf85977881e0159d20314d6e838688d6558b5
GregSlazinski <esenthel@hotmail.com>
2020-11-25 21:43:50 +0700

    ok

--- ThirdPartyLibs/Tiff/Windows/clean.bat
+++ ThirdPartyLibs/Tiff/Windows/clean.bat
@@ -9,9 +9,21 @@ del Release\*.obj
 del Release\*.log
 rmdir /s /q Release\tiff.tlog
 
+del "Release Universal\*.obj"
+del "Release Universal\*.log"
+del "Release Universal\*.recipe"
+del "Release Universal\*.txt"
+rmdir /s /q "Release Universal\tiff.tlog"
+
 del x64\Release\*.obj
 del x64\Release\*.log
 rmdir /s /q x64\Release\tiff.tlog
+
+del "x64\Release Universal\*.obj"
+del "x64\Release Universal\*.log"
+del "x64\Release Universal\*.recipe"
+del "x64\Release Universal\*.txt"
+rmdir /s /q "x64\Release Universal\tiff.tlog"
 
 del ARM\Release\*.obj
 del ARM\Release\*.log
