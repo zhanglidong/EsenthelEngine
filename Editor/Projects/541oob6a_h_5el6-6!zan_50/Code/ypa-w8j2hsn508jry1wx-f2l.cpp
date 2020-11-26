015b159a4ee16b817032469dc1529b451479a840
GregSlazinski <esenthel@hotmail.com>
2020-11-26 20:01:23 +0700

    ok

--- Editor/Projects/541oob6a_h_5el6-6!zan_50/Code/ypa-w8j2hsn508jry1wx-f2l.cpp
+++ Editor/Projects/541oob6a_h_5el6-6!zan_50/Code/ypa-w8j2hsn508jry1wx-f2l.cpp
@@ -39,9 +39,49 @@ void Shut()
 {
 }
 /******************************************************************************/
+cchar8* TextOK(bool ok) {return ok ? "SUCCESS" : "FAIL";}
+void TestCloud()
+{
+   Str msg;
+
+   // save
+   {
+      Str text="Text";
+      File f; f.writeMem().putStr(text).pos(0);
+      msg.line()+=S+"Save \""+text+"\": "+TextOK(XboxLive.cloudSave("save", f));
+   }
+   // load
+   {
+      File f;
+      bool ok=XboxLive.cloudLoad("save", f, true); f.pos(0);
+      Str  text=f.getStr();
+      msg.line()+=S+"Load: "+TextOK(ok)+", text: \""+text+"\"";
+   }
+   // list files
+   {
+      Memt<XBOXLive.CloudFile> files;
+      XboxLive.cloudFiles(files);
+      msg.line()+=S+"Cloud Files: "+files.elms();
+   }
+   // delete
+   {
+      msg.line()+=S+"Delete File: "+TextOK(XboxLive.cloudDel("save"));
+   }
+   // list files
+   {
+      Memt<XBOXLive.CloudFile> files;
+      XboxLive.cloudFiles(files);
+      msg.line()+=S+"Cloud Files: "+files.elms();
+   }
+
+   Gui.msgBox("XboxLive Cloud Test Results", msg);
+}
+/******************************************************************************/
 bool Update()
 {
+   Gui.update();
    if(Kb.bp(KB_ESC))return false;
+   if(Kb.bp(KB_ENTER))TestCloud();
    return true;
 }
 /******************************************************************************/
@@ -57,11 +97,14 @@ void Draw()
       case XBOXLive.LOGGED_IN : status="Logged In" ; break;
    }
    D.text(0, 0.9, status);
+
    if(XboxLive.status()==XBOXLive.LOGGED_IN)
    {
       D.text(0, 0.8, S+"Welcome "+XboxLive.userName());
       if(ImagePtr photo=IC.getImage(XboxLive.userImageURL()))photo->drawFit(Rect_U(0, 0.7, 0.3, 0.3));
+
       D.text(0, 0.3, S+"Cloud supported:"+XboxLive.cloudSupported()+", available size:"+XboxLive.cloudAvailableSize());
+
       D.text(0, 0.2, S+"Friends:");
       flt x=-D.w()/2, y=0.1;
       Memt<XBOXLive.Friend> friends; if(XboxLive.getFriends(friends))
@@ -74,6 +117,9 @@ void Draw()
             if(ImagePtr photo=IC.getImage(user.image_url))photo->drawFit(Rect_R(x-0.05, y, 0.1, 0.1));
          }
       }else D.text(0, y, "Unknown");
+
+      D.text(0, -0.9, "Press Enter to test Cloud Saves");
    }
+   Gui.draw();
 }
 /******************************************************************************/
