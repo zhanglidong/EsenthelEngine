cf740052e5dd3ec669967c808bc288b1ec5119d6
GregSlazinski <esenthel@hotmail.com>
2021-01-28 17:07:44 +0700

    ok

--- ThirdPartyLibs/PhysX/!Extract Linux OBJ Files.bat
+++ ThirdPartyLibs/PhysX/!Extract Linux OBJ Files.bat
@@ -6,4 +6,6 @@ cd obj
 ..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXCharacterKinematic_static_64.a
 ..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXCommon_static_64.a
 ..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXCooking_static_64.a
+..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXExtensions_static_64.a
 ..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXFoundation_static_64.a
+..\..\..\..\..\..\..\Engine\Android\cygwin\ar.exe -x ..\libPhysXVehicle_static_64.a
