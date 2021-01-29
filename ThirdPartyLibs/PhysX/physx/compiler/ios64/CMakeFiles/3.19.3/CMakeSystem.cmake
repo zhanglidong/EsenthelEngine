987e0996c471890bcd8c7ff7b58adc0a301b2114
GregSlazinski <esenthel@hotmail.com>
2021-01-29 15:13:53 +0700

    ok

--- ThirdPartyLibs/PhysX/physx/compiler/ios64/CMakeFiles/3.19.3/CMakeSystem.cmake
+++ ThirdPartyLibs/PhysX/physx/compiler/ios64/CMakeFiles/3.19.3/CMakeSystem.cmake
@@ -5,9 +5,9 @@ set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/Applications/Esenthel/ThirdPartyLibs/PhysX/externals/cmakemodules/ios/ios.toolchain.cmake")
 
-set(CMAKE_SYSTEM "Darwin-14.3")
+set(CMAKE_SYSTEM "Darwin-14.4")
 set(CMAKE_SYSTEM_NAME "Darwin")
-set(CMAKE_SYSTEM_VERSION "14.3")
+set(CMAKE_SYSTEM_VERSION "14.4")
 set(CMAKE_SYSTEM_PROCESSOR "")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
