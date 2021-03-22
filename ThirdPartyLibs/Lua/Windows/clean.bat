c07eb308c74d1089f85e28ad89d24d9b6ce7b19b
GregSlazinski <esenthel@hotmail.com>
2021-03-22 18:40:51 +0700

    ok

---
+++ ThirdPartyLibs/Lua/Windows/clean.bat
@@ -0,0 +1,37 @@
+rmdir /s /q .vs
+rmdir /s /q Debug
+rmdir /s /q "Generated Files"
+
+del *.sdf
+del *.VC.db
+
+del Release\*.obj
+del Release\*.log
+del Release\*.tlog
+del Release\*.lastbuildstate
+del Release\*.txt
+rmdir /s /q Release\lua.tlog
+
+del x64\Release\*.obj
+del x64\Release\*.log
+del x64\Release\*.txt
+rmdir /s /q x64\Release\lua.tlog
+
+del "ARM\Release\*.obj"
+del "ARM\Release\*.log"
+del "ARM\Release\*.resfiles"
+del "ARM\Release\*.txt"
+del "ARM\Release\*.xml"
+del "ARM\Release\*.intermediate"
+del "ARM\Release\*.recipe"
+rmdir /s /q "ARM\Release\Lua"
+rmdir /s /q "ARM\Release\lua.tlog"
+rmdir /s /q "ARM\Release\embed"
+
+del Emscripten\Release\*.o
+del Emscripten\Release\*.log
+del Emscripten\Release\*.tlog
+del Emscripten\Release\*.lastbuildstate
+del Emscripten\Release\*.cache
+
+rmdir /s /q NX64
