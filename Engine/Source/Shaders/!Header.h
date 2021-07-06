b6eb97595bd4810cf2320fdd8ab0a1939ecbe3d6
GregSlazinski <esenthel@hotmail.com>
2021-07-06 18:04:37 +0700

    ok

--- Engine/Source/Shaders/!Header.h
+++ Engine/Source/Shaders/!Header.h
@@ -147,8 +147,8 @@
 #define MS_SAMPLES 4 // number of samples in multi-sampled render targets
 
 // signed formats in GL depend on "GL_EXT_render_snorm"
-#define SIGNED_NRM_RT 0 // if Normal     Render Target  is  signed, never  because we use IMAGE_R10G10B10A2 which is unsigned
-#define SIGNED_MTN_RT 0 // if MotionBlur Render Targets are signed, never  because we use IMAGE_R10G10B10A2 which is unsigned
+#define SIGNED_NRM_RT 0 // if Normal     Render Target  is  signed, never because we use IMAGE_R10G10B10A2 which is unsigned
+#define SIGNED_MTN_RT 0 // if MotionBlur Render Targets are signed, never because we use IMAGE_R10G10B10A2 which is unsigned
 
 #define REVERSE_DEPTH (!GL) // if Depth Buffer is reversed. Can't enable on GL because for some reason (might be related to #glClipControl) it disables far-plane depth clipping, which can be observed when using func=FUNC_ALWAYS inside 'D.depthFunc'. Even though we clear the depth buffer, there may still be performance hit, because normally geometry would already get clipped due to far plane, but without it, per-pixel depth tests need to be performed.
 #if     REVERSE_DEPTH
@@ -176,11 +176,11 @@
    #define TexDepthRawMin(uv)  TexMin(Depth, uv).x
    #define TexDepthRawMax(uv)  TexMax(Depth, uv).x
 #endif
-/******************************************************************************/
-// TEXTURE ACCESSING                 (Y^)
-// GATHER returns in following order: V1 X  Y
-//                                    V0 W  Z
-//                                     + U0 U1 (X>)
+/******************************************************************************
+TEXTURE ACCESSING                 (Y^)
+GATHER returns in following order: V1 X  Y
+                                   V0 W  Z
+                                    + U0 U1 (X>)
 /******************************************************************************/
 #define Tex(    image, uv )   image.Sample(SamplerDefault, uv ) // access a 2D   texture
 #define Tex3D(  image, uvw)   image.Sample(SamplerDefault, uvw) // access a 3D   texture
