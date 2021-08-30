e8e362b0693cfc76951e218a81b932e8ca3256a2
GregSlazinski <esenthel@hotmail.com>
2021-08-30 20:13:31 +0700

    ok

--- Editor Source/_Build_/Esenthel/Source/Gui/Theater.cpp
+++ Editor Source/_Build_/Esenthel/Source/Gui/Theater.cpp
@@ -251,6 +251,7 @@ p_scale=&add("Item 3D Scale"          , MemberDesc(MEMBER(Options, item_3d_scale
                if(options.draw_axis)m.scaleOrn(options.axis_size).draw();
             }break;
 
+            case ELM_ICON :
             case ELM_IMAGE: if(ImagePtr image=Proj.gamePath(elm->id))
             {
                Rect image_rect=image->fit(rect);
@@ -352,7 +353,7 @@ p_scale=&add("Item 3D Scale"          , MemberDesc(MEMBER(Options, item_3d_scale
                 this_contains_name=(FlagTest(child.flag, ELM_CONTAINS_NAME) || parent_contains_name); // for performance reasons, merge 'this_contains_name' with 'parent_contains_name'
                child_contains_name= FlagTest(child.flag, ELM_CONTAINS_NAME_CHILD);
             }
-            if(elm.type==elm_type && this_contains_name)
+            if(ElmCompatible(elm.type, elm_type) && this_contains_name)
             {
                switch(options.show)
                {
